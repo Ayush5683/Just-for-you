6d1403ee2ca44007387c8e271b6d13d38cef002c
Ayush5683 <141903522+Ayush5683@users.noreply.github.com>
2025-03-13 22:44:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function playAudio() {
+    let audio = document.getElementById("audio");
+    audio.play();
+    document.getElementById("sorry-text").style.opacity="1";
+    document.getElementById("sorry-text").style.bottom="-80px";
+    document.getElementById("sorry-text").style.scale="1.2";
+    document.getElementById("use-button").style.opacity="0";
+}
